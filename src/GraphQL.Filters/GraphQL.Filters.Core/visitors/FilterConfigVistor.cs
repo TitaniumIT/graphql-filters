3ae6940bb5bc6cf63ab0d6b6901a26672314a52d
Marco Fuijkschot <marcof@titaniumit.nl>
2023-11-03 08:53:43 +0100

    fixing

--- src/GraphQL.Filters/GraphQL.Filters.Core/visitors/FilterConfigVistor.cs
+++ src/GraphQL.Filters/GraphQL.Filters.Core/visitors/FilterConfigVistor.cs
@@ -11,7 +11,7 @@ namespace nl.titaniumit.graphql.filters.visitors;
 internal class FilterConfigVistor : BaseSchemaNodeVisitor
 {
     ScalarConverterService _converterService;
-    
+
     public FilterConfigVistor(IServiceProvider serviceProvider)
     {
         _converterService = serviceProvider.GetRequiredService<ScalarConverterService>();
@@ -21,33 +21,40 @@ internal class FilterConfigVistor : BaseSchemaNodeVisitor
     {
         base.VisitObject(type, schema);
     }
-    
+
     public override void VisitObjectFieldDefinition(FieldType field, IObjectGraphType type, ISchema schema)
     {
         var graphType = field.ResolvedType?.GetNamedType();
-        if (graphType != null &&  graphType.GetType().Assembly != typeof(ISchema).Assembly)
+        if (graphType != null && graphType.GetType().Assembly != typeof(ISchema).Assembly)
         {
             if (graphType.IsLeafType() &&
               !schema.BuiltInTypeMappings.Any(x => x.graphType == graphType.GetType()))
             {
-                var clrType = schema.TypeMappings.Where(x => x.graphType == graphType.GetType()).Select(x => x.clrType).Single();
-                _converterService.Add(clrType, graphType as ScalarGraphType ?? throw new InvalidOperationException());
-            }
-            if(  (field.Type?.IsAssignableTo(typeof(ListGraphType))??false) && graphType is IObjectGraphType objecType )
-            {
-                if( schema.IsFilterType( type.ClrType() ))
+                var clrType = schema.TypeMappings.Where(x => x.graphType == graphType.GetType()).Select(x => x.clrType).SingleOrDefault();
+                if (clrType != null)
                 {
-                    if( !type.ClrType().GetMembers(BindingFlags.Public|BindingFlags.Instance).Any( m => m.Name.ToCamelCase() == field.Name))
+                    _converterService.Add(clrType, graphType as ScalarGraphType ?? throw new InvalidOperationException());
+                }
+                else
+                {
+                    throw new InvalidOperationException($"clrType Mapping not found for {graphType.Name} of {type.Name}.{field.Name}");
+                }
+            }
+            if ((field.Type?.IsAssignableTo(typeof(ListGraphType)) ?? false) && graphType is IObjectGraphType objecType)
+            {
+                if (schema.IsFilterType(type.ClrType()))
+                {
+                    if (!type.ClrType().GetMembers(BindingFlags.Public | BindingFlags.Instance).Any(m => m.Name.ToCamelCase() == field.Name))
                     {
-                       var any = schema.AllTypes.SingleOrDefault( t => t.Name == $"AnyGraphType{type.ClrType().Name}" ) as IInputObjectGraphType;
-                       if ( any != null )
-                       {
-                         var ft = FieldBuilderExtentions.CreateAnyField(field.Name , objecType.ClrType());
-                         ft.ResolvedType = schema.AllTypes.SingleOrDefault( t => t.Name == $"FilterGraphType{objecType.ClrType().Name}" );
-                         ft.Metadata["collectionType"] =objecType.ClrType();
-                         any.AddField( ft);
-                         any.Metadata[field.Name] = $"path:{type.Name}.{field.Name}";
-                       }
+                        var any = schema.AllTypes.SingleOrDefault(t => t.Name == $"AnyGraphType{type.ClrType().Name}") as IInputObjectGraphType;
+                        if (any != null)
+                        {
+                            var ft = FieldBuilderExtentions.CreateAnyField(field.Name, objecType.ClrType());
+                            ft.ResolvedType = schema.AllTypes.SingleOrDefault(t => t.Name == $"FilterGraphType{objecType.ClrType().Name}");
+                            ft.Metadata["collectionType"] = objecType.ClrType();
+                            any.AddField(ft);
+                            any.Metadata[field.Name] = $"path:{type.Name}.{field.Name}";
+                        }
                     }
                 }
             }
