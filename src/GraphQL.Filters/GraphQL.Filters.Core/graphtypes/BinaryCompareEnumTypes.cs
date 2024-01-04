57eeb6331f9f53f28de8fb9fe7ba9ad9be79dee9
Marco Fuijkschot <marcof@titaniumit.nl>
2024-01-04 12:17:20 +0100

    Equal on object types are only reference checked #37

--- src/GraphQL.Filters/GraphQL.Filters.Core/graphtypes/BinaryCompareEnumTypes.cs
+++ src/GraphQL.Filters/GraphQL.Filters.Core/graphtypes/BinaryCompareEnumTypes.cs
@@ -9,24 +9,11 @@ public class BinaryCompareEnumTypes : EnumerationGraphType
 {
     public BinaryCompareEnumTypes()
     {
-        Add("equal",
-             new BinaryCompareType(ExpressionType.Equal, (ConditionType condition, Expression arg, IResolveFieldContext ctx) => Binary(condition, arg, ctx, Expression.Equal)));
-        Add("greater",
-             new BinaryCompareType(ExpressionType.GreaterThan, (ConditionType condition, Expression arg, IResolveFieldContext ctx) => Binary(condition, arg, ctx, Expression.GreaterThan)));
-        Add("greaterOrEqual",
-            new BinaryCompareType(ExpressionType.GreaterThanOrEqual, (ConditionType condition, Expression arg, IResolveFieldContext ctx) => Binary(condition, arg, ctx, Expression.GreaterThanOrEqual)));
-        Add("less",
-            new BinaryCompareType(ExpressionType.LessThan, (ConditionType condition, Expression arg, IResolveFieldContext ctx) => Binary(condition, arg, ctx, Expression.LessThan)));
-        Add("lessOrEqual",
-            new BinaryCompareType(ExpressionType.LessThanOrEqual, (ConditionType condition, Expression arg, IResolveFieldContext ctx) => Binary(condition, arg, ctx, Expression.LessThanOrEqual)));
-        Add("notEqual",
-            new BinaryCompareType(ExpressionType.NotEqual, (ConditionType condition, Expression arg, IResolveFieldContext ctx) => Binary(condition, arg, ctx, Expression.NotEqual)));
-    }
-
-    static Expression Binary(ConditionType condition, Expression arg, IResolveFieldContext ctx, Func<Expression, Expression, BinaryExpression> _factory)
-    {
-        var val = condition.Value(ctx);
-        var eq = _factory.Invoke(condition.GetMemberExpression(arg, ctx) ?? throw new NullReferenceException(), val);
-        return eq;
+        Add("equal", new BinaryCompareType(ExpressionType.Equal));
+        Add("greater", new BinaryCompareType(ExpressionType.GreaterThan));
+        Add("greaterOrEqual", new BinaryCompareType(ExpressionType.GreaterThanOrEqual));
+        Add("less", new BinaryCompareType(ExpressionType.LessThan));
+        Add("lessOrEqual", new BinaryCompareType(ExpressionType.LessThanOrEqual));
+        Add("notEqual", new BinaryCompareType(ExpressionType.NotEqual));
     }
 }
