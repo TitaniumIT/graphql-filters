9343c0b874c0246b741478560d9aa4e1d5aa98ef
Marco Fuijkschot <marcof@titaniumit.nl>
2024-01-19 14:41:38 +0100

    added or checks

--- src/GraphQL.Filters/GraphQL.Filters.Extentions/sql/ast/AstNode.cs
+++ src/GraphQL.Filters/GraphQL.Filters.Extentions/sql/ast/AstNode.cs
@@ -1,6 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace nl.titaniumit.graphql.filters.extentions.sql.ast;
 
-
+[ExcludeFromCodeCoverage]
 internal abstract record AstNode
 {
     internal abstract string ToSql();
